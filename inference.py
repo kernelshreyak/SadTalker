ad731b5900fcb760c94aad96e65460598f059733
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2024-03-02 15:52:03 +0530

    updated inference

--- inference.py
+++ inference.py
@@ -57,58 +57,63 @@ def main(args):
     if currentPendingCount == -1 :
         currentPendingCount = 0
     r.set('pending-tasks',str(int(currentPendingCount) + 1))
+    try:
 
-    print('3DMM Extraction for source image')
-    first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
-                                                                             source_image_flag=True, pic_size=args.size)
-    if first_coeff_path is None:
-        print("Can't get the coeffs of the input")
-        return
+        print('3DMM Extraction for source image')
+        first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
+                                                                                source_image_flag=True, pic_size=args.size)
+        if first_coeff_path is None:
+            print("Can't get the coeffs of the input")
+            return
 
-    if ref_eyeblink is not None:
-        ref_eyeblink_videoname = os.path.splitext(os.path.split(ref_eyeblink)[-1])[0]
-        ref_eyeblink_frame_dir = os.path.join(save_dir, ref_eyeblink_videoname)
-        os.makedirs(ref_eyeblink_frame_dir, exist_ok=True)
-        print('3DMM Extraction for the reference video providing eye blinking')
-        ref_eyeblink_coeff_path, _, _ =  preprocess_model.generate(ref_eyeblink, ref_eyeblink_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_eyeblink_coeff_path=None
-
-    if ref_pose is not None:
-        if ref_pose == ref_eyeblink: 
-            ref_pose_coeff_path = ref_eyeblink_coeff_path
+        if ref_eyeblink is not None:
+            ref_eyeblink_videoname = os.path.splitext(os.path.split(ref_eyeblink)[-1])[0]
+            ref_eyeblink_frame_dir = os.path.join(save_dir, ref_eyeblink_videoname)
+            os.makedirs(ref_eyeblink_frame_dir, exist_ok=True)
+            print('3DMM Extraction for the reference video providing eye blinking')
+            ref_eyeblink_coeff_path, _, _ =  preprocess_model.generate(ref_eyeblink, ref_eyeblink_frame_dir, args.preprocess, source_image_flag=False)
         else:
-            ref_pose_videoname = os.path.splitext(os.path.split(ref_pose)[-1])[0]
-            ref_pose_frame_dir = os.path.join(save_dir, ref_pose_videoname)
-            os.makedirs(ref_pose_frame_dir, exist_ok=True)
-            print('3DMM Extraction for the reference video providing pose')
-            ref_pose_coeff_path, _, _ =  preprocess_model.generate(ref_pose, ref_pose_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_pose_coeff_path=None
+            ref_eyeblink_coeff_path=None
 
-    #audio2ceoff
-    batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=args.still)
-    coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+        if ref_pose is not None:
+            if ref_pose == ref_eyeblink: 
+                ref_pose_coeff_path = ref_eyeblink_coeff_path
+            else:
+                ref_pose_videoname = os.path.splitext(os.path.split(ref_pose)[-1])[0]
+                ref_pose_frame_dir = os.path.join(save_dir, ref_pose_videoname)
+                os.makedirs(ref_pose_frame_dir, exist_ok=True)
+                print('3DMM Extraction for the reference video providing pose')
+                ref_pose_coeff_path, _, _ =  preprocess_model.generate(ref_pose, ref_pose_frame_dir, args.preprocess, source_image_flag=False)
+        else:
+            ref_pose_coeff_path=None
 
-    # 3dface render
-    if args.face3dvis:
-        from src.face3d.visualize import gen_composed_video
-        gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
-    
-    #coeff2video
-    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
-                                batch_size, input_yaw_list, input_pitch_list, input_roll_list,
-                                expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size)
-    
-    result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info, \
-                                enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
-    
-    shutil.move(result, save_dir+'.mp4')
-    print('The generated video is named:', save_dir+'.mp4')
+        #audio2ceoff
+        batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=args.still)
+        coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
 
-    r.set(jobId, 'DONE')
-    r.set('pending-tasks',str(int(currentPendingCount) - 1))
-    print("-----------------Job completed:" + jobId)
+        # 3dface render
+        if args.face3dvis:
+            from src.face3d.visualize import gen_composed_video
+            gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
+        
+        #coeff2video
+        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
+                                    batch_size, input_yaw_list, input_pitch_list, input_roll_list,
+                                    expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size)
+        
+        result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info, \
+                                    enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
+        
+        shutil.move(result, save_dir+'.mp4')
+        print('The generated video is named:', save_dir+'.mp4')
+
+        r.set(jobId, 'DONE')
+        r.set('pending-tasks',str(int(currentPendingCount) - 1))
+        print("-----------------Job completed:" + jobId)
+    except Exception as e:
+        r.set(jobId, 'FAILED')
+        r.set('pending-tasks',str(int(currentPendingCount) - 1))
+        print("-----------------Job failed:" + jobId)
 
     if not args.verbose:
         shutil.rmtree(save_dir)
